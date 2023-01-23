5d583f345e6b8bfd3a9d2a30e2ce64b52df41746
Aishwarya Pathak <pathakaishwarya54@gmail.com>
2023-01-23 23:34:11 +0530

    Added keyboard functionalities

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -2,26 +2,17 @@ import React, { useContext } from "react";
 import { AppContext } from "../App";
 
 function Key({ keyVal, bigKey}) {
-  const { board,setBoard, currAttempt, setCurrAttempt } = useContext(AppContext);
+  const { onSelectLetter, onDelete, onEnter} = useContext(AppContext);
 
   const selectLetter = () => {
     if (keyVal==="ENTER"){
-        if (currAttempt.letterPos !== 5) return;
-        setCurrAttempt({attempt: currAttempt.attempt+1, letterPos: 0})
+        onEnter();
     }
     else if (keyVal==="DELETE"){
-        if (currAttempt.letterPos ===0) return;
-        const newBoard= [...board];
-        newBoard[currAttempt.attempt][currAttempt.letterPos-1]="";
-        setBoard(newBoard);
-        setCurrAttempt({...currAttempt,letterPos : currAttempt.letterPos-1});
+        onDelete();
     }
     else{
-        if (currAttempt.letterPos >4) return;
-        const newBoard= [...board];
-        newBoard[currAttempt.attempt][currAttempt.letterPos]=keyVal;
-        setBoard(newBoard);
-        setCurrAttempt({...currAttempt,letterPos : currAttempt.letterPos+1});
+        onSelectLetter(keyVal);
     }
   };
   return (
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React , {useContext, useEffect} from 'react';
+import { useCallback } from 'react';
+import { AppContext } from '../App';
 import Key from './Key';
 
 function Keyboard() {
+  const { onSelectLetter, onDelete, onEnter} = useContext(AppContext);
+
   const KEY1= ["Q","W","E","R","T","Y","U","I","O","P"];
   const KEY2= ["A","S","D","F","G","H","J","K","L"];
   const KEY3= ["Z","X","C","V","B","N","M"];
+
+  const handleKeyboard=useCallback((event)=>{
+    if(event.key==="Enter"){
+      onEnter();
+    }
+    else if(event.key==="Backspace"){
+      onDelete();
+    }
+    else{
+      KEY1.forEach((k)=>{
+        if(event.key.toLowerCase() === k.toLowerCase()){
+          onSelectLetter(k);
+        }
+      });
+      KEY2.forEach((k)=>{
+        if(event.key.toLowerCase() === k.toLowerCase()){
+          onSelectLetter(k);
+        }
+      });
+      KEY3.forEach((k)=>{
+        if(event.key.toLowerCase() === k.toLowerCase()){
+          onSelectLetter(k);
+        }
+      });
+    }
+  });
+  useEffect(()=>{
+    document.addEventListener("keydown",handleKeyboard);
+    return ()=>{
+      document.removeEventListener("keydown",handleKeyboard);
+    };
+  },[handleKeyboard]);
   return (
     <>
-      <div className="keyboard">
+      <div className="keyboard" onKeyDown={handleKeyboard}>
             <div className="line1">
               {KEY1.map((k)=>(<Key keyVal={k}/>))}
               </div>
